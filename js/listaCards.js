47518e8316208d51c4b80cd538285373e2703804
Wagner Oliveira <contato-wagneros@outlook.com>
2024-11-20 12:24:31 -0300

    Revisão
    Adicionado o texto "Nenhum produto adicionado" quando não houver itens no carrinho!

--- js/listaCards.js
+++ js/listaCards.js
@@ -4,6 +4,13 @@ const cardContainer = document.querySelector(".card-container");
 export function criarCard(nome, valor, imagem, id) {
     const card = document.createElement("div");
     card.classList.add("card-container--info");
+
+     // Exclua o elemento h3 presente no HTML
+     const h3Element = cardContainer.querySelector("h3");
+     if (h3Element) {
+        h3Element.remove();
+     }
+
     card.innerHTML = `
         <img class="card-container--image" src="${imagem}" alt="${nome}">
         <p>${nome}</p>
